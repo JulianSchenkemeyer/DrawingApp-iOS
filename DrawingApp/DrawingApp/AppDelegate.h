263deda34c750001e46dfac1705f48fd5b3d2f7e
Julian <J.schenkem@gmail.com>
2015-01-16 10:26:08 +0100

    initial Commit

---
+++ DrawingApp/DrawingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingApp
+//
+//  Created by Julian Schenkemeyer on 16/01/15.
+//  Copyright (c) 2015 SchenkemeyerJulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingApp/DrawingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingApp
+//
+//  Created by Julian Schenkemeyer on 16/01/15.
+//  Copyright (c) 2015 SchenkemeyerJulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
