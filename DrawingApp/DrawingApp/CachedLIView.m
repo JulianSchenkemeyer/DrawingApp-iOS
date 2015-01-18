6ab1dfa8ce4fe5095f6e39f9e3e14ae0d9206afb
Julian <Julian.schenkemeyer@gmail.com>
2015-01-18 14:09:57 +0100

    drawing process was outsourced to its own thread

--- DrawingApp/DrawingApp/CachedLIView.m
+++ DrawingApp/DrawingApp/CachedLIView.m
@@ -9,21 +9,24 @@
 #import "CachedLIView.h"
 
 #define FUDGE_FACTOR 100
+#define BUFFCAP 100
 
 @implementation CachedLIView
 {
-    UIBezierPath *path;
     UIImage *incrementalImage;  // offscreen bitmap for storing a copy of our screen
     CGPoint pts[5];             // four points to create a Bezier segment + one point to smooth out the junction point
     uint ctr;                   // counter to keep track of point index
+
+    CGPoint pointsBuffer[BUFFCAP];
+    uint buffIndex;
+    dispatch_queue_t drawingQueue;
 }
 
 - (id)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
-        [self setMultipleTouchEnabled:NO];          // We only want the user to be able to draw with one finger
-        path = [UIBezierPath bezierPath];
-        [path setLineWidth:2.0];                    // thickness
+        [self setMultipleTouchEnabled:NO];                          // We only want the user to be able to draw with one finger
+        drawingQueue = dispatch_queue_create("drawingQueue", NULL); // create the drawingQueue
     }
     return self;
 }
@@ -36,6 +39,7 @@
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     ctr = 0;                                    // set counter to 0
+    buffIndex = 0;
     UITouch *touch = [touches anyObject];
     pts[0] = [touch locationInView:self];       // location
 }
@@ -50,43 +54,62 @@
     if (ctr == 4) {                                                                 // if 5 point are available
         
         pts[3] = CGPointMake((pts[2].x + pts[4].x)/2.0, (pts[2].y + pts[4].y)/2.0); // find the middle between the second controlpoint and the first controlpoint of the next bezier curve
+        pointsBuffer[buffIndex] = pts[0];
+        pointsBuffer[buffIndex + 1] = pts[1];
+        pointsBuffer[buffIndex + 2] = pts[2];
+        pointsBuffer[buffIndex + 3] = pts[3];
         
-        [path moveToPoint:pts[0]];                                                  // set beginning of the Bezier curve
-        [path addCurveToPoint:pts[3] controlPoint1:pts[1] controlPoint2:pts[2]];    // draw the Bezier curve from pts[0] to pts[3]
+        buffIndex += 4;
         
-        // create an offscreen bitmap
-        UIGraphicsBeginImageContextWithOptions(self.bounds.size, YES, 0.0);
-        
-        // if no offscreen bitmap is already available, create one
-        if (!incrementalImage) {
-            UIBezierPath *rectpath = [UIBezierPath bezierPathWithRect:self.bounds];
-            [[UIColor whiteColor] setFill];
-            [rectpath fill];
-        }
-        
-        [incrementalImage drawAtPoint:CGPointZero];
-        [[UIColor blackColor] setStroke];
-        
-        // change stroke width in relation to drawing speed
-        float speed = 0.0;
-        
-        // calculate the drawing speed
-        for (int i = 0; i < 3; i++) {
-            float dx = pts[i+1].x - pts[i].x;
-            float dy = pts[i+1].y - pts[i].y;
-            speed += sqrtf(dx * dx + dy * dy);
-        }
-        
-        float width = FUDGE_FACTOR / speed;
-        
-        [path setLineWidth:width];
-        [path stroke];
-        incrementalImage = UIGraphicsGetImageFromCurrentImageContext();
-        
-        [self setNeedsDisplay];                                                     // refresh
+        CGRect bounds = self.bounds;
+        dispatch_async(drawingQueue, ^{
+            if (buffIndex == 0) {
+                return;
+            }
+            UIBezierPath *path = [UIBezierPath bezierPath];
+            for (int i = 0; i < buffIndex; i += 4) {
+                // draw bezier curve
+                [path moveToPoint:pointsBuffer[i]];
+                [path addCurveToPoint:pointsBuffer[i+3] controlPoint1:pointsBuffer[i+1] controlPoint2:pointsBuffer[i+2]];
+            }
+            
+            // create an offscreen bitmap
+            UIGraphicsBeginImageContextWithOptions(bounds.size, YES, 0.0);
+            
+            // if no offscreen bitmap is already available, create one
+            if (!incrementalImage) {
+                UIBezierPath *rectpath = [UIBezierPath bezierPathWithRect:bounds];
+                [[UIColor whiteColor] setFill];
+                [rectpath fill];
+            }
+            
+            [incrementalImage drawAtPoint:CGPointZero];
+            [[UIColor blackColor] setStroke];
+            
+            // change stroke width in relation to drawing speed
+            float speed = 0.0;
+            
+            // calculate the drawing speed
+            for (int i = 0; i < 3; i++) {
+                float dx = pts[i+1].x - pts[i].x;
+                float dy = pts[i+1].y - pts[i].y;
+                speed += sqrtf(dx * dx + dy * dy);
+            }
+            
+            float width = FUDGE_FACTOR / speed;
+            
+            [path setLineWidth:width];
+            [path stroke];
+            incrementalImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                buffIndex = 0;
+                [self setNeedsDisplay];
+            });
+        });
         
         // setup for the next curve
-        [path removeAllPoints];
         pts[0] = pts[3];
         pts[1] = pts[4];
         ctr = 1;
